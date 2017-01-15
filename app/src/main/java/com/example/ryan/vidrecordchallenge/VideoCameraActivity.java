499fa2cf159c5c99d6a1d8dd585b75fe83c6b9d1
Ryan <ryan.a.truong@outlook.com>
2017-01-15 09:59:12 -0800

    initial creation

---
+++ app/src/main/java/com/example/ryan/vidrecordchallenge/VideoCameraActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryan.vidrecordchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_camera);
+    }
+}
